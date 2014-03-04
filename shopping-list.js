bbe1dd7dd85ddfc5da86364b390444d59e60c96c
Ian Valentine <ian@ivalentine.net>
2014-03-03 20:26:24 -0500

    Many changes

--- shopping-list.js
+++ shopping-list.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
 
 	var	$form = $("#addform"),
-		$list = $("#list"),
+		$list = $("#itemlist"),
 		$add = $("#add-item"),
-		$item = $("#new-item");
+		$edit = $("#edit-item"),
+		$remove = $("#remove-item"),
+		$item = $("#new-item"),
+		$checked = $("input:checked"),
+		$error = $("#error");
 
 	$form.on("submit", function(e){
 		e.preventDefault();
@@ -12,39 +16,66 @@ $(document).ready(function() {
 		var itemNameLowerCase = itemName.toLowerCase();
 	
 		if (itemName.length) {
-			$list.append("<input type='checkbox' name='item' value='" + itemNameLowerCase + "' />" + itemName + "<br />");
+			$list.append("<span class='item'><input type='checkbox' name='item' value='" + itemNameLowerCase + "' /><span>" + itemName + "</span></span>");
 			$item.val("");
+			$error.hide();
 		}
 		else {
-			console.log("Please enter a valid item name.");
+			$error.show();
 			$item.val("");
 		}
 	});
 	
-	$add.on('click', function(e){
+	$add.on("click", function(e){
 			e.preventDefault();
-			/*
-			var itemName = $.trim($item.val());
-	
-			if (itemName.length) {
-				$list.append("<li>" + itemName + "</li>");
-				$item.val("");
-			}
-			else {
-				console.log("Please enter a valid item name.");
-				$item.val("");
-			}
-			*/
 			$form.submit();
 		});
+		
+	$remove.on("click", function(e){
+			e.preventDefault();
+			$(".checked-item").each(function() {
+				$(this).parent().remove();
+			});
+		});
+		
+	$edit.on("click", function(e){
+			e.preventDefault();
+			if ($(this).text() == "Edit") {
+				$(this).text("Done");
+				$(".item").each(function() {
+					$(this).removeClass("item").addClass("item-editing");
+					var itemtext = $(this).find("span").text();
+					$(this).find("span").hide();
+					$(this).append('<input type="text" class="newitemtext" value="' + itemtext + '" required>');
+				});
+			}
+			else {
+				$(this).text("Edit");
+				$(".item-editing").each(function() {
+					$(this).removeClass("item-editing").addClass("item");
+					var itemtext = $(this).find(".newitemtext").val();
+						$(this).find(".newitemtext").remove();
+						$(this).find("span").text(itemtext).show();
+				});
+			}
+	});
 
-	$("input[type=checkbox]").on("click", function() {
-		$(this).toggleClass("checked-item");
+	$("#itemlist").on("click", "input[type=checkbox]", function(e) {
+		e.preventDefault();
+		console.log(e);
+	});
+
+	$("#itemlist").on("click", ".item", function(e) {
+		var input = $(this).find("input");
+		if (input.is(":checked")) {
+			input.prop("checked", false);
+			console.log("test");
+		} else {
+			input.prop("checked", true);
+		}
+		$(this).find("span").toggleClass("checked-item");
+		$edit.prop("disabled", true);										/* 	when item is checked, disable button
+																				when no items are checked, enable button */
 		});
 
-	/*		
-	$list.on('click', 'li', function(){
-   		$(this).toggleClass('checked-item');
-	});
-	*/
 })
